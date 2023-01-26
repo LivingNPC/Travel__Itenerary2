4408eb7894504f3d9e4ac5438a8ef57d841d2383
LAI MING YUEN 2101411C <2101411C@student.tp.edu.sg>
2023-01-26 13:38:13 +0800

    flights index.razor

---
+++ Travel__Itenerary2/Client/Static/Endpoints.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Travel__Itenerary2.Client.Static
+{
+    public static class Endpoints
+    {
+
+        private static readonly string Prefix = "api";
+
+        public static readonly string FlightsEndpoint = $"{Prefix}/Flights";
+        public static readonly string HotelsEndpoint = $"{Prefix}/Hotels";
+        public static readonly string PackageEndpoint = $"{Prefix}/Package";
+        public static readonly string CustomersEndpoint = $"{Prefix}/Customers";
+        public static readonly string PaymentsEndpoint = $"{Prefix}/Payments";
+        public static readonly string StaffEndpoint = $"{Prefix}/Staff";
+        public static readonly string BookingsEndpoint = $"{Prefix}/Bookings";
+
+    }
+}
+
